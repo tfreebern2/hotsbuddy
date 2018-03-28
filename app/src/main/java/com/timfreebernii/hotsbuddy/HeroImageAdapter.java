4e1ea7dd423a2525bc9f9c801597dc3b780258aa
Timothy Freebern II <tfreebern2@gmail.com>
2018-03-28 19:55:50 -0400

    Resized Hero Thumbnails. Gride setup in 3 columns

--- app/src/main/java/com/timfreebernii/hotsbuddy/HeroImageAdapter.java
+++ app/src/main/java/com/timfreebernii/hotsbuddy/HeroImageAdapter.java
@@ -1,9 +1,11 @@
 package com.timfreebernii.hotsbuddy;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 public class HeroImageAdapter extends BaseAdapter {
@@ -25,14 +27,17 @@ public class HeroImageAdapter extends BaseAdapter {
         return 0;
     }
 
+
     public View getView(int position, View convertView, ViewGroup parent) {
 
         ImageView heroView;
 
+
+
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             heroView = new ImageView(mContext);
-            heroView.setLayoutParams(new ViewGroup.LayoutParams(85, 85));
+            heroView.setLayoutParams(new ViewGroup.LayoutParams(240, 240));
             heroView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             heroView.setPadding(8, 8, 8, 8);
         } else {
@@ -44,16 +49,15 @@ public class HeroImageAdapter extends BaseAdapter {
     }
 
     private Integer[] mThumbIds = {
-            R.drawable.abathur_thumb, R.drawable.alarak_thumb, R.drawable.alexstrasza_thumb,
-            R.drawable.ana_thumb, R.drawable.anubarak_thumb, R.drawable.artanis_thumb,
-            R.drawable.arthas_thumb, R.drawable.auriel_thumb, R.drawable.azmodan_thumb,
-            R.drawable.blaze_thumb, R.drawable.brightwing_thumb, R.drawable.cassia_thumb,
-            R.drawable.chen_thumb, R.drawable.cho_thumb, R.drawable.chromie_thumb,
-            R.drawable.dva_thumb, R.drawable.dehaka_thumb, R.drawable.diablo_thumb, R.drawable.etc_thumb,
-            R.drawable.falstad_thumb, R.drawable.fenix_thumb, R.drawable.gall_thumb, R.drawable.garrosh_thumb,
-            R.drawable.gazlowe_thumb, R.drawable.genji_thumb, R.drawable.greymane_thumb, R.drawable.guldan_thumb,
-            R.drawable.hanzo_thumb, R.drawable.illidan_thumb, R.drawable.jaina_thumb, R.drawable.johanna_thumb,
-            R.drawable.junkrat_thumb, R.drawable.kaelthas_thumb, R.drawable.kelthuzad_thumb, R.drawable.kerrigan_thumb,
+            R.drawable.abathur_thumb, R.drawable.alarak_thumb, R.drawable.alexstrasza_thumb, R.drawable.ana_thumb,
+            R.drawable.anubarak_thumb, R.drawable.artanis_thumb, R.drawable.arthas_thumb, R.drawable.auriel_thumb,
+            R.drawable.azmodan_thumb, R.drawable.blaze_thumb, R.drawable.brightwing_thumb, R.drawable.cassia_thumb,
+            R.drawable.chen_thumb, R.drawable.cho_thumb, R.drawable.chromie_thumb, R.drawable.dva_thumb,
+            R.drawable.dehaka_thumb, R.drawable.diablo_thumb, R.drawable.etc_thumb, R.drawable.falstad_thumb,
+            R.drawable.fenix_thumb, R.drawable.gall_thumb, R.drawable.garrosh_thumb, R.drawable.gazlowe_thumb,
+            R.drawable.genji_thumb, R.drawable.greymane_thumb, R.drawable.guldan_thumb, R.drawable.hanzo_thumb,
+            R.drawable.illidan_thumb, R.drawable.jaina_thumb, R.drawable.johanna_thumb, R.drawable.junkrat_thumb,
+            R.drawable.kaelthas_thumb, R.drawable.kelthuzad_thumb, R.drawable.kerrigan_thumb,
             R.drawable.kharazim_thumb, R.drawable.leoric_thumb, R.drawable.li_ming_thumb, R.drawable.li_li_thumb,
             R.drawable.lt_morales_thumb, R.drawable.lunara_thumb, R.drawable.lucio_thumb, R.drawable.maiev_thumb,
             R.drawable.malfurion_thumb, R.drawable.malthael_thumb, R.drawable.medivh_thumb, R.drawable.muradin_thumb,
