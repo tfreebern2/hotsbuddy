202d61e7e79165a2d99d02a8f5a7812870201de9
Timothy Freebern II <tfreebern2@gmail.com>
2018-04-02 20:16:28 -0400

    Work in progress.

--- app/src/main/java/com/timfreebernii/hotsbuddy/HeroListActivity.java
+++ app/src/main/java/com/timfreebernii/hotsbuddy/HeroListActivity.java
@@ -7,28 +7,64 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class HeroListActivity extends AppCompatActivity {
+
+    final String HEROES_URL = "https://hotsapi.net/api/v1";
+
+    HeroListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hero_list);
+        heroListAPI(HEROES_URL);
 
         GridView gridview = (GridView) this.findViewById(R.id.lvHeroes);
         gridview.setAdapter(new HeroImageAdapter(this));
 
+//        ListView listView = (ListView) findViewById(R.id.lvHeroes);
+//        heroes = new ArrayList<HeroDataModel>();
+//        adapter = new HeroListAdapter(this, heroes);
+//        listView.setAdapter(adapter);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(HeroListActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+    }
+
+    private void heroListAPI(String url) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                ArrayList<HeroDataModel> myHeroes = HeroDataModel.fromJsonToModelList(response);
+
+                Log.d("HoTS", "Hero: " + myHeroes);
+
+                adapter.clear();
+                adapter.addAll(myHeroes);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONArray response) {
+                Log.e("HoTS", "Fail " + e.toString());
+                Log.d("HoTS", "Status code " + statusCode);
+                Toast.makeText(HeroListActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ app/src/main/java/com/timfreebernii/hotsbuddy/HeroListAdapter.java
@@ -0,0 +1,41 @@
+package com.timfreebernii.hotsbuddy;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class HeroListAdapter extends ArrayAdapter<HeroDataModel> {
+
+    public HeroListAdapter(HeroListActivity context, ArrayList<HeroDataModel> heroes) {
+        super(context, 0, heroes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        HeroDataModel currentHero = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.hero_list_item, parent, false);
+        }
+
+
+        TextView heroNameView = convertView.findViewById(R.id.hero_name);
+
+//        TextView heroImageView = convertView.findViewById(R.id.hero_image);
+
+//        TextView heroGroupView = convertView.findViewById(R.id.hero_group);
+
+        heroNameView.setText(currentHero.getHeroName());
+//        heroImageView.setText(currentHero.getHeroImage());
+//        heroGroupView.setText(currentHero.getHeroGroup());
+
+        return convertView;
+    }
+
+}
